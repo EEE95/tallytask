7d4437ee5effe3375a52334ddf0348f5705371e3
EEE95 <144793581+EEE95@users.noreply.github.com>
2024-11-21 08:53:36 +0100

    Kommentarer til Confirm og Date

--- src/component/Confirm.tsx
+++ src/component/Confirm.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { ConfirmProps } from "../types";
 import "../css/style.css";
 
+// Component to render a confimation dialog
 const Confirm: React.FC<ConfirmProps> = ({ message, onConfirm, onCancel}) => {
     return (
         <div className="confirm-overlay">
+            
+            {/* Main content of the confirmation dialog */}
             <div className="confirm-content">
                 <p>{message}</p>
+
+                 {/* Buttons for confirm/cancel the action */}
                 <div className="confirm-actions">
                     <button onClick={onConfirm} className="confirm-btn">OK</button>
                     <button onClick={onCancel} className="cancel-btn">Cancel</button>
--- src/component/Date.tsx
+++ src/component/Date.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+// function to generate today's date
 function getDate() {
   const today = new Date();
   const month = today.getMonth() + 1;
@@ -8,7 +9,9 @@ function getDate() {
   return `${date}/${month}/${year}`;
 }
 
+// Display today's date
 function TodaysDay() {
+  // State to store the current date
   const [currentDate, setCurrentDate] = useState(getDate());
 
   return (
